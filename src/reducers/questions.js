5340af949d175c54181979ada6630b8276b6ec97
SamuelT12321 <62050366+SamuelT12321@users.noreply.github.com>
2020-06-07 22:14:46 +0800

    Quick fix one typo

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -2,7 +2,7 @@ import { RECEIVE_QUESTIONS } from '../actions/questions'
 
 export default function questions (state = {}, action) {
 	switch(action.type){
-		case RECEIVE_QUESTIONS
+		case RECEIVE_QUESTIONS:
 		return {	
 			...state,
 			...action.questions
