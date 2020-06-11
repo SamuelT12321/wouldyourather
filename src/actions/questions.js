101f67fe20c4752bcc7b9f7f0bf2476f36a6e961
SamuelT12321 <62050366+SamuelT12321@users.noreply.github.com>
2020-06-12 00:24:19 +0800

    Add Submit Function to QuestionsCardAnswer.js
    -add submit fucntion that call action.js
    -added working radio button with onchange fucntions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,38 @@
+import { saveQuestionAnswer } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION_ANSWER = 'ADD_QUESTION_ANSWER'
+
+function addQuestionAnswer ({authedUser, qid, answer}) {
+	return {
+		type: ADD_QUESTION_ANSWER,
+		authedUser,
+		qid,
+		answer
+	}
+}
+
+export function handleAddQuestionAnswer (info){
+	return (dispatch, getState)=>{
+		const { authedUser } = getState()
+		const { questionId, answer } = info
+		dispatch(showLoading())
+
+		return saveQuestionAnswer({
+			authedUser,
+			qid: questionId,
+			answer
+		})
+		.then((question)=> dispatch(addQuestionAnswer(info)))
+		.then(()=>dispatch(hideLoading()))
+	}
+}
 
 export function receiveQuestions (questions) {
 	return {
 		type: RECEIVE_QUESTIONS,
 		questions,
 	}
-
 }
+
--- src/components/QuestionCardAnswer.js
+++ src/components/QuestionCardAnswer.js
@@ -1,41 +1,84 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatQuestion } from '../utils/helpers'
+import { handleAddQuestionAnswer } from '../actions/questions'
 
 export class QuestionCardAnswer extends Component {
+    state={
+        answer:'',//optionOne or optionTwo
+        userid:'',
+        questionid:'',
+        selectedOption:'',
+    }
+    handleChange = (e) => {
+
+        const selectedOption = e.target.value
+        this.setState(() => ({
+            selectedOption
+        }))
+      }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { dispatch, question } =this.props
+        const { id } = question
+        const answer = this.state.selectedOption
+        alert("answer :" + answer +",  id :"+ id)
+        //qid, answer
+        dispatch(handleAddQuestionAnswer({
+            qid:id,
+            answer 
+        }))
+        
+        //answer can be optionOne or optionTwo
+        //update users[authedUser].answers = ...object.add object.property { questionid : answer} 
+        //update question[id].answer.votes.concat(authedUser)
+    }
     render() {
-         //const { question } = this.props;
-        // const { avatar, id, name, optionOneText, optionTwoText } = question
-        //console.log("quesiton : " + quesiton);
+        console.log(this.props);
+        const { question } = this.props;
+        const { avatar, id, name, optionOneText, optionTwoText } = question
+
         return (
-            <div className="row">
+            <form className="row" onSubmit={this.handleSubmit}>
                 <div className="col s8 offset-s2">
                     <div className="card blue-grey darken-1">
-                        {/* <div className="card-content white-text">
+                        <div className="card-content white-text">
                         <img className="imgcenter" src={avatar}/>
                         <span className="card-title">{name} ask would you rather</span>
                         <ul className="collection">
                             <li className="collection-item">
                                 <label>
-                                    <input type="radio" name="group1"/>
+                                    <input type="radio" 
+                                           name="group1" 
+                                           value="optionOne"
+                                           checked={this.state.selectedOption === "optionOne"}
+                                           onChange={this.handleChange}
+                                           />
                                     <span>{optionOneText}</span>
                                 </label>
                             </li>
                             <li className="collection-item">
                                 <label>
-                                    <input type="radio" name="group1"/>
+                                    <input type="radio" 
+                                           name="group1" 
+                                           value="optionTwo"
+                                           checked={this.state.selectedOption === "optionTwo"}
+                                           onChange={this.handleChange}
+                                           />
                                     <span>{optionTwoText}</span>
                                 </label>
                             </li>
                         </ul>
-                        </div> */}
+                        </div>
                         <div className="card-action center-align">
-                        <a className="" href="#">Submit</a>
+                            <button className={`btn waves-effect waves-light ${this.state.selectedOption===''? "disabled" : ''}`}
+                                    type="submit">
+                                    Submit
+                            </button>
                         </div>
                     </div>
                 </div>
-            </div>
-
+            </form>
         )
     }
 }
@@ -49,7 +92,9 @@ function mapStateToProps({authedUser, users, questions},{id}) {
     console.log("id : "+id)
     return{
         authedUser,
-        //question: formatQuestion(quesiton, users[quesiton.author],authedUser)
+        question: !questions[id]
+        ? []
+        : formatQuestion(quesiton, users[quesiton.author],authedUser),
     }
 }
 
